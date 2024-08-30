4558354f4ac17790dfda6ad623f737477547b672
achrafidrissi <achrafidrissi05@gmail.com>
2024-08-30 02:26:26 +0100

    Fullstack App NextJs-React

---
+++ app/meals/loading-out.jsx
@@ -0,0 +1,9 @@
+// -out <=> Ignored
+
+import classes from './loading.module.css'
+
+export default function MealsLoadingPage(){
+    return (
+        <p className={classes.loading} >Fetching  meals...</p>
+    )
+}
